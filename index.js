10b78dc644ba1ca8b3d43a2ea6f7d098beb694d4
Sanskar Suri <sanskarsuri50@gmail.com>
2024-09-02 15:57:13 +0530

    removed nodemon and added PORT env variable

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const mongoose=require('mongoose')
 const transactionsRoutes=require('./routes/transactions')
+const PORT=process.env.PORT || 3000
 
 const app = express()
 
@@ -14,7 +15,7 @@ app.use(transactionsRoutes)
 mongoose.connect(process.env.MONGO_KEY)
 .then(res=>{
     console.log('Connected to MongoDB')
-    app.listen(3000)
+    app.listen(PORT)
 }).catch(err=>{
     console.log(err)
 })
